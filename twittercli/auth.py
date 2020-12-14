011338314b21d6780e7f0556a98632a425ab8250
Stiven Roytman <stevo@archpad.com>
2020-12-13 21:25:41 -0500

    Secret should now be stored in package directory rather than current directory.

--- twittercli/auth.py
+++ twittercli/auth.py
@@ -1,5 +1,6 @@
 from getpass import getpass
 from os import system
+from . import get_data
 
 def manualAuth():
     userLogin = input('Username: ')
@@ -7,18 +8,21 @@ def manualAuth():
     return [userLogin, userPass]
 
 def storeSecret(userLogin, userPass):
+    secretFilePath = get_data('secret.gpg')
     print('Please enter your GPG user ID (it usually looks like an email address).')
     gpgID = input('GPG ID: ')
     printfCall = 'printf "%s\n%s" "{}" "{}"'.format(userLogin, userPass.replace('$', '\\$'))
-    gpgCall = 'gpg -r {} --encrypt > .secret.gpg'.format(gpgID)
+    gpgCall = 'gpg -r {} --encrypt > {}'.format(gpgID,secretFilePath)
     systemCall = ' | '.join([printfCall, gpgCall])
     system(systemCall)
 
 # Retrieving secret
 def retrieveSecret():
-    system('mkfifo .secret')
-    system('gpg --decrypt .secret.gpg > .secret &')
-    with open('.secret', 'r') as secretFile:
-        secret = [secretData.strip() for secretData in secretFile.readlines()]
-    system('rm .secret')
+    secretPipePath = get_data('secret')
+    secretFilePath = get_data('secret.gpg')
+    system('mkfifo {}'.format(secretPipePath))
+    system('gpg --decrypt {} > {} &'.format(secretFilePath,secretPipePath))
+    with open(secretPipePath, 'r') as secretPipe:
+        secret = [secretData.strip() for secretData in secretPipe.readlines()]
+    system('rm {}'.format(secretPipePath))
     return secret
